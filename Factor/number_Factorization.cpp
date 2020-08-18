7597a0e439a84ea293400c3059486be207d105a0
VACHARA VIRIYAKUL <63010871@KMITL.AC.TH>
2020-08-18 17:20:39 +0700

    Add project files.

---
+++ Factor/number_Factorization.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+	int x, i = 2, k = 0;
+	printf("Enter number : ");
+	scanf_s("%d", &x);
+	printf("Factoring Result : ");
+	while (x != 1)
+	{
+		while (x%i == 0)
+		{
+			printf("%d", i);
+			x /= i;
+			if (x!=1)
+			{
+				printf(" x ");
+			}
+		}
+		i++;
+	}
+	return 0;
+}
