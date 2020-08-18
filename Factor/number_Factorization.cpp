8c2ae35ddd4a6fa2c5b5c4f1f050fc7e66ca5bb6
VACHARA VIRIYAKUL <63010871@KMITL.AC.TH>
2020-08-18 17:36:29 +0700

    edit variable name

--- Factor/number_Factorization.cpp
+++ Factor/number_Factorization.cpp
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main() 
 {
-	int x, i = 2, k = 0;
+	int a, i = 2, k = 0;
 	printf("Enter number : ");
-	scanf_s("%d", &x);
+	scanf_s("%d", &a);
 	printf("Factoring Result : ");
-	while (x != 1)
+	while (a != 1)
 	{
-		while (x%i == 0)
+		while (a%i == 0)
 		{
 			printf("%d", i);
-			x /= i;
-			if (x!=1)
+			a /= i;
+			if (a!=1)
 			{
 				printf(" x ");
 			}
